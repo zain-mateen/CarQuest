99fb10874261c1c11984aacc663909fba3319117
Muhammad Shahzaib Zahid <dev.muhammad.shahzaib@gmail.com>
2025-01-08 20:29:28 +0500

    video section updated

--- src/components/VideoSec/VideoSec.js
+++ src/components/VideoSec/VideoSec.js
@@ -1,40 +1,20 @@
-import React, { useRef, useState } from 'react';
-import { Play, Pause } from 'phosphor-react';
+import React from 'react';
+import { Play } from 'phosphor-react';
 
 import SectionTitle from '../SectionTitle/SectionTitle';
 import VideoSlider from './VideoSlider';
 import { PartnerLogos } from '../../Data';
 
-import BackgroundVideo from '../../images/hero-video.mp4'
+import Image from '../../images/video-img.png'
+import { Link } from 'react-router-dom';
 
 const VideoSec = () => {
-    const videoRef = useRef(null);
-    const [isPlaying, setIsPlaying] = useState(false);
-
-    const handlePlayPause = () => {
-        if (videoRef.current) {
-            if (isPlaying) {
-                videoRef.current.pause();
-            } else {
-                videoRef.current.play();
-            }
-            setIsPlaying(!isPlaying);
-        }
-    };
-
-    const handleVideoEnd = () => {
-        setIsPlaying(false);
-    };
-
     return (
         <div className="overflow-hidden relative">
-            <video 
-                ref={videoRef} 
-                className="animate-fade absolute top-0 left-0 -z-[1] w-full h-full object-cover" 
-                src={BackgroundVideo} 
-                controls={false}
-                muted 
-                onEnded={handleVideoEnd}
+            <img 
+                className='animate-fade absolute top-0 left-0 -z-[1] w-full h-full object-cover' 
+                src={Image} 
+                alt="" 
             />
             <div className="absolute top-0 left-0 bg-black w-full h-full opacity-40" />
             <div className="max-w-[700px] mx-auto relative z-2 py-24 max-xl:py-20 max-m:py-12">
@@ -48,16 +28,13 @@ const VideoSec = () => {
                     headingStyles="video-heading text-white"
                 />
                 <div className="animate-fade flex justify-center mt-8">
-                    <button 
+                    <Link 
                         className="flex items-center justify-center w-20 h-20 bg-primary rounded-full duration-300 ease-in-out cursor-pointer"
-                        onClick={handlePlayPause}
+                        to='https://www.youtube.com/'
+                        target='_blank'
                     >
-                        {isPlaying ? (
-                            <Pause size={32} weight="fill" className="text-white" />
-                        ) : (
-                            <Play size={32} weight="fill" className="text-white" />
-                        )}
-                    </button>
+                        <Play size={32} weight="fill" className="text-white" />
+                    </Link>
                 </div>
             </div>
             <div className='animate-fade border-t border-white/[25%]'>
